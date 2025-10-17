4dc53a9e0c0bac1c0651fa2ad803bbe3d31e1c54
devogs <billy.bob@devogs.xyz>
2025-10-17 20:47:53 +0200

    FEAT: Add coverage file and cleanup unused files

--- app/hash.py
+++
@@ -1,8 +0,0 @@
-import bcrypt
-
-plain_password = "admin1234"
-
-hashed_password = bcrypt.hashpw(plain_password.encode('utf-8'), bcrypt.gensalt())
-
-print("Mot de passe haché :")
-print(hashed_password.decode('utf-8'))
